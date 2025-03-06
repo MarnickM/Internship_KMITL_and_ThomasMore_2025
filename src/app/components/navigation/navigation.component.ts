09a550274b2da1f0e7dcef26d46c47cd1570176c
Marnick Michielsen <113896267+MarnickM@users.noreply.github.com>
2025-03-06 15:01:46 +0700

    navbar made and dynamic topics

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
 
+  profileMenuOpen: boolean = false;
+
+  toggleProfileMenu() {
+    this.profileMenuOpen = !this.profileMenuOpen;
+  }
 }
--- src/app/pages/writer/topic-overview/topic-overview.component.ts
+++ src/app/pages/writer/topic-overview/topic-overview.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../components/button/button.component";
+import { TopicService } from "../../../services/topics/topic-service.service";
+import { Topic } from '../../../services/topics/topic';
+
 
 @Component({
   selector: 'app-topic-overview',
@@ -10,4 +13,13 @@ import { ButtonComponent } from "../../../components/button/button.component";
 })
 export class TopicOverviewComponent {
 
+  topics: Topic[] | undefined;
+
+  constructor(private topicService: TopicService) { }
+
+  ngOnInit() {
+    this.topicService.getTopics().subscribe(topics => {
+      this.topics = topics;
+    });
+  }
 }
