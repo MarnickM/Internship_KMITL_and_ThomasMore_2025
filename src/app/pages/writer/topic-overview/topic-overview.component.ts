d9c10dda53b0fc5e358666bf3e699fcd976c1579
Marnick Michielsen <113896267+MarnickM@users.noreply.github.com>
2025-03-07 11:28:42 +0700

    made numbers drawing page

--- src/app/pages/writer/topic-overview/topic-overview.component.ts
+++ src/app/pages/writer/topic-overview/topic-overview.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../components/button/button.component";
 import { TopicService } from "../../../services/topics/topic-service.service";
 import { Topic } from '../../../services/topics/topic';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,11 +16,18 @@ export class TopicOverviewComponent {
 
   topics: Topic[] | undefined;
 
-  constructor(private topicService: TopicService) { }
+  constructor(private topicService: TopicService, private router: Router) { }
 
   ngOnInit() {
     this.topicService.getTopics().subscribe(topics => {
       this.topics = topics;
     });
+  }
+
+  navigateToDrawing(topic: Topic) {
+    this.router.navigate(['/drawing'], {
+      queryParams: { id: topic.id, name: topic.name }
+    });
+    
   }
 }
