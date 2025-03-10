a5a2489464f36668b2dce9ec4447dd8d0ccebf01
sendewael <r0856234@student.thomasmore.be>
2025-03-10 08:25:01 +0100

    the user gets to see diferent pages on login based on the role

---
+++ src/app/pages/manager/topic-management/topic-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './topic-management.component.html',
+  styleUrl: './topic-management.component.css'
+})
+export class TopicManagementComponent {
+
+}
---
+++ src/app/pages/manager/writer-management/writer-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-writer-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './writer-management.component.html',
+  styleUrl: './writer-management.component.css'
+})
+export class WriterManagementComponent {
+
+}
