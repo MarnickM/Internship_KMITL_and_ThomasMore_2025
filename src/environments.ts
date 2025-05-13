3b78babc8d3b65c927f35dd76f18e8b599b70944
Marnick Michielsen <113896267+MarnickM@users.noreply.github.com>
2025-05-13 09:15:29 +0700

    Add files via upload

---
+++ src/environments.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    google_client_id: "REPLACE_WITH_YOUR_ID",
+    google_clientsecret: "REPLACE_WITH_YOUR_SECRET",
+    writer_role_id: "dfaNnrXgH6bqFys0Sw44",
+    manager_role_id: "UEjEnMYFPoKvwMOauVnY",
+    admin_role_id: "rnBKQzZVTlsqyOqufFq5",
+    no_role: "No Role",
+    favicon_url: "https://www.google.com/imgres?q=black%20and%20white%20pencil&imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1396842794%2Fvector%2Fedit-line-icon-design-with-editable-stroke-suitable-for-web-page-mobile-app-ui-ux-and-gui.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DRn-fwGy1rf6PPZl51ygH-7sSBCZJPNb97Wsh_blbUtc%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fillustrations%2Fpencil-clipart-black-and-white&docid=fu_JLC5U5t9-SM&tbnid=zmRTZCPATx3w3M&vet=12ahUKEwjDg-HMh_-LAxVuR2wGHcYwN5wQM3oECEMQAA..i&w=612&h=612&hcb=2&ved=2ahUKEwjDg-HMh_-LAxVuR2wGHcYwN5wQM3oECEMQAA"
+};
